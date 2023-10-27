6620dabd0339751b2f15726d09e7191f980616e4
Astralstr1ke <99681023+Astralstr1ke@users.noreply.github.com>
2023-10-27 08:11:05 +0200

    int

--- Program.cs
+++ Program.cs
@@ -1,51 +1,61 @@
+var builder = WebApplication.CreateBuilder(args);
 
-namespace WebApplication1
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IstudentRepository, StudentRepository>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//endpoints
+app.MapPost("/student", (Student std, IstudentRepository sr) =>
+{ sr.Add(std); });
+
+app.MapGet("/student/{id}", (Guid id, IstudentRepository sr) => { sr.Get(id); });
+
+
+
+app.Run();
+
+public interface IstudentRepository
+{
+    public void Add(Student student);
+    public Student Get(Guid id);
+
+}
+public class StudentRepository : IstudentRepository
+{
+    private List<Student> _students;
+    public StudentRepository()
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
-
-            app.Run();
-        }
+        _students = new List<Student>();
     }
+
+    public void Add(Student std)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Student Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Student
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Major { get; set; }
+
+
+
 }
